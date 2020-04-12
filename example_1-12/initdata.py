0725209768261becbf4c281bb1d6fcd845016986
mala23 <mail@mala23.me>
2020-04-12 13:32:35 +0200

    dump shelve

---
+++ example_1-12/initdata.py
@@ -0,0 +1,17 @@
+# inits data to be stored
+
+# records
+bob = {'name': 'Bob Smith', 'age': 42, 'pay': 3000, 'job': 'dev'}
+sue = {'name': 'Sue Jones', 'age': 45, 'pay': 4000, 'job': 'hdw'}
+tom = {'name': 'Tom', 'age': 50, 'pay': 0, 'job': None}
+
+# database
+db = {}
+db['bob'] = bob
+db['sue'] = sue
+db['tom'] = tom
+
+if __name__ == '__main__':
+# when run as a script
+    for key in db:
+        print(key, '=>\n ', db[key])
